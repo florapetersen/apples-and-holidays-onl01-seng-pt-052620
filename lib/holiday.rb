5fda3fc52c3fc37998f262a806ea8a9b1d04a348
Flora Petersen <floramarina@comcast.net>
2020-06-06 23:45:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supply|
-      if holiday == christmas
+      if holiday == "christmas"
         supply << "Balloons"
       end
     end
