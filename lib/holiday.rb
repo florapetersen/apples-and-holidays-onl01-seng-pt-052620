466929ae894dd9a6a5c3e448a8f6a98fc6da4083
Flora Petersen <floramarina@comcast.net>
2020-06-07 00:07:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,6 +48,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+      puts "#{holiday}.to_s.split.map {|i| i.capitalize}
+
+
 
 end
 
